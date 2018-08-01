9ed74767de011bd20b4bfd9304754e6b15df22b3
David Guan <davidguan@canva.com>
2018-08-01 17:13:58 +1000

    test

--- public/canva-test.js
+++ public/canva-test.js
@@ -2,9 +2,9 @@
 wx.config({
   debug: false,
   appId: 'wxd8c042e1754c001b',
-  timestamp: 'TODO',
-  nonceStr: 'TODO',
-  signature: 'TODO',
+  timestamp: '1533107564899',
+  nonceStr: '8f5e0918-83f8-4017-8b76-b8e2731c0487',
+  signature: '6836afa047d57ebd22946f81cd2dfdad044e94de',
   jsApiList: [
     'onMenuShareAppMessage',
     'onMenuShareTimeline',
