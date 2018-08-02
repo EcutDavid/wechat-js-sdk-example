74af575987ef92a363c65ab7521a92ef40230386
David Guan <davidguan@canva.com>
2018-08-02 17:40:32 +1000

    new test

--- public/canva-test-4.js
+++ public/canva-test-4.js
@@ -1,7 +1,7 @@
 
 wx.config({
   debug: false,
-  appId: 'wx994773dc132fcf88',
+  appId: 'wx114e537b6f541f2f',
   timestamp: '1533195419523',
   nonceStr: '1b164f2b-b01e-482c-8d32-a4d67d993a1a',
   signature: '444043e54626c343c840020de99a697fd24e7aaf',
